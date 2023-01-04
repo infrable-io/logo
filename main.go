9977688bf502d6529d68070151bdac5e0e577283
nickolashkraus <0x@nickolaskraus.io>
2023-01-04 15:48:51 -0600

    Add copyright

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// Copyright 2023 Infrable. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
 package main
 
 import "github.com/infrable-io/logo/cmd"
